3bce5af213a71b512aa190795c648fe1e14eacbc
unknown <sittiolioolioolio@gmail.com>
2026-03-29 14:17:53 +0700

    fix load

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Lottie from "lottie-react";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -14,17 +14,8 @@ const queryClient = new QueryClient();
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [animationData, setAnimationData] = useState(null);
 
   useEffect(() => {
-    // fetch lottie dari link
-    fetch(
-      "https://lottie.host/8ad54d76-6f2a-41f3-9b93-62364473640a/aKWsMLqeno.lottie"
-    )
-      .then((res) => res.json())
-      .then((data) => setAnimationData(data));
-
-    // durasi loading (bisa kamu ubah)
     const timer = setTimeout(() => {
       setLoading(false);
     }, 2500);
@@ -36,11 +27,12 @@ const App = () => {
   if (loading) {
     return (
       <div className="w-full h-screen flex items-center justify-center bg-black">
-        {animationData && (
-          <div className="w-60 h-60">
-            <Lottie animationData={animationData} loop={true} />
-          </div>
-        )}
+        <Player
+          autoplay
+          loop
+          src="https://lottie.host/8ad54d76-6f2a-41f3-9b93-62364473640a/aKWsMLqeno.lottie"
+          style={{ height: "200px", width: "200px" }}
+        />
       </div>
     );
   }
@@ -54,7 +46,6 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
